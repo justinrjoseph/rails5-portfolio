61523dacf262940f4d3e869a76f496880d8ac54c
nsiderwolverine <justin.joseph0@yahoo.com>
2017-02-15 02:36:14 +0000

    Custom home, about, and contact routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  resources :portfolio_items
-  get 'pages/home'
-
-  get 'pages/about'
-
-  get 'pages/contact'
+  resources :portfolio_items_path
+  
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'pages#home'
 end
