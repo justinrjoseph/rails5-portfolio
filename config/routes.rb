fcf400da739f479447db8f10b37c839a09416573
nsiderwolverine <justin.joseph0@yahoo.com>
2017-02-21 00:02:06 +0000

    Rename portfolio items model and customize portfolio show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :portfolio_items_path
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170212214007) do
+ActiveRecord::Schema.define(version: 20170220234940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20170212214007) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "portfolio_items", force: :cascade do |t|
+  create_table "portfolios", force: :cascade do |t|
     t.string   "title"
     t.string   "subtitle"
     t.text     "body"
