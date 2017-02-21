7c2d5e653146196ed6f38689acf7b0a298eb1b73
nsiderwolverine <justin.joseph0@yahoo.com>
2017-02-21 23:15:10 +0000

    Add validations for blog, portfolio, and skill properties

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,9 @@
 class Blog < ApplicationRecord
   enum status: { draft: 0, published: 1 }
-  extend FriendlyId
   
+  extend FriendlyId
   friendly_id :title, use: :slugged
+  
+  validates :title, presence: true
+  validates :body, presence: true
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,6 @@
 class Portfolio < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :image, presence: true
+  validates :thumbnail, presence: true
 end
