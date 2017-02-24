85ca886ffbe43681ed09d25496b443d516e6cf02
nsiderwolverine <justin.joseph0@yahoo.com>
2017-02-24 02:17:45 +0000

    Add name attribute to user accounts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  before_filter :augment_permitted_params, if: :devise_controller?
+  
+  private
+  
+    def augment_permitted_params
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
 end
