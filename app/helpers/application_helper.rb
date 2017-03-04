a9c344d6f2ee0c186cc1831bc7b22472856fb432
nsiderwolverine <justin.joseph0@yahoo.com>
2017-03-04 18:46:46 +0000

    Add copyright generator via custom gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag :p, greeting, class: 'source-greeting'
     end
   end
+  
+  def copyright_generator
+    ITYViewTool::Renderer.copyright 'Justin Joseph', 'All rights reserved.'
+  end
 end
