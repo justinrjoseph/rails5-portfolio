cafc34d3f251a23a4e41145cdf10c01f5ea9381e
nsiderwolverine <justin.joseph0@yahoo.com>
2017-03-11 03:45:22 +0000

    Refactor navigation and apply active class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,25 @@
 module ApplicationHelper
   def login_helper(style = '')
     if current_user.is_a?(GuestUser)
-      (link_to 'Register', new_user_registration_path, class: style) +
+      (link_to 'Register', new_user_registration_path, class: style) + ' ' +
       (link_to 'Sign In', new_user_session_path, class: style)
     else
       link_to 'Log Out', destroy_user_session_path, class: style, method: :delete
     end
+  end
+  
+  def nav_helper(style, tag_1 = '', tag_2 = '')
+    nav_links = ''
+    
+    nav_items.each do |nav_item|
+      nav_links << tag_1
+      nav_links << "<a href='#{nav_item[:url]}' "
+      nav_links << "class='#{style}#{active?(nav_item[:url])}'>"
+      nav_links << "#{nav_item[:text]}</a>"
+      nav_links << tag_2
+    end
+    
+    nav_links.html_safe
   end
   
   def source_helper
@@ -18,4 +32,35 @@ module ApplicationHelper
   def copyright_generator
     ITYViewTool::Renderer.copyright 'Justin Joseph', 'All rights reserved.'
   end
+  
+  private
+  
+    def nav_items
+      [
+        {
+          url: root_path,
+          text: 'Home'
+        },
+        {
+          url: about_path,
+          text: 'About'
+        },
+        {
+          url: contact_path,
+          text: 'Contact'
+        },
+        {
+          url: blogs_path,
+          text: 'Blog'
+        },
+        {
+          url: portfolios_path,
+          text: 'Portfolio'
+        }
+      ]
+    end
+  
+    def active?(path)
+      " active" if current_page?(path)
+    end
 end
