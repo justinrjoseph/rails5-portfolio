29c8d0b3abb9479b8116e0d3c630e8928be980f1
nsiderwolverine <justin.joseph0@yahoo.com>
2017-03-15 00:04:26 +0000

    Update file uploader styles and refactor placeholder code

--- app/models/concerns/placeholder.rb
+++
@@ -1,7 +0,0 @@
-module Placeholder
-  extend ActiveSupport::Concern
-  
-  def self.image_generator(h:, w:)
-    "http://placehold.it/#{h}x#{w}"
-  end
-end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,11 +1,6 @@
 class Portfolio < ApplicationRecord
-  include Placeholder
-  after_initialize :set_defaults
-  
   validates :title, presence: true
   validates :body, presence: true
-  validates :image, presence: true
-  validates :thumbnail, presence: true
   
   has_many :technologies
   accepts_nested_attributes_for :technologies, 
@@ -23,11 +18,4 @@ class Portfolio < ApplicationRecord
   end
   
   scope :ruby_on_rails, ->{ where(subtitle: 'Ruby on Rails') }
-  
-  private
-  
-    def set_defaults
-      self.image ||= Placeholder.image_generator(h: '600', w: '400')
-      self.thumbnail ||= Placeholder.image_generator(h: '350', w: '200')
-    end
 end
