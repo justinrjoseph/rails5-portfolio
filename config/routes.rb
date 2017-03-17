c349c1a93b9e9bbe3ca866a3249c43e34eccbaf3
nsiderwolverine <justin.joseph0@yahoo.com>
2017-03-17 23:53:21 +0000

    Add tweets to tech news page

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
     put :sort, on: :collection
   end
   
-  get 'portfolio/:id' => 'portfolios#show', as: 'portfolio_show'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   
-  get 'about' => 'pages#about'
-  get 'contact' => 'pages#contact'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'tech-news', to: 'pages#tech_news' 
 
   resources :blogs do
     member do
